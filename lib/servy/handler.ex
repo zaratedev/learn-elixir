9d2b58638529a96ffc47a8ece401bc5628784b58
Jonathan Zarate <zaratedev@gmail.com>
2024-04-23 15:03:44 -0600

    Slicing And Dicing With Enum

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -153,7 +153,7 @@ POST /bears HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
-Content-Type: application/x-www-form-urlencoded
+Content-Type: multipart/form-data
 Content-Length: 21
 
 name=Baloo&type=Brown
