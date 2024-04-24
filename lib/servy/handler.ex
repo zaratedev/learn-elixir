455ff20fd0b6d71e0ca604975d091bdf30182a34
Jonathan Zarate <zaratedev@gmail.com>
2024-04-24 17:36:30 -0600

    Install Poison

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -28,6 +28,10 @@ defmodule Servy.Handler do
     %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
+  def route(%Conv{ method: "GET", path: "/api/bears" } = conv) do
+    Servy.Api.BearController.index(conv)
+  end
+
   def route(%Conv{ method: "GET", path: "/bears" } = conv) do
     BearController.index(conv)
   end
@@ -56,7 +60,7 @@ defmodule Servy.Handler do
     # TODO: Use values in the map to create an HTTP response
     """
     HTTP/1.1 #{Conv.full_status(conv)}\r
-    Content-Type: text/html\r
+    Content-Type: #{conv.resp_content_type}\r
     Content-Length: #{String.length(conv.resp_body)}\r
     \r
     #{conv.resp_body}
