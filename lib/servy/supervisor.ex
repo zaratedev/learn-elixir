fbde1e49745b4ba77b38f1abb87a4864def6a5a2
Jonathan Zarate <zaratedev@gmail.com>
2024-04-29 15:46:38 -0600

    Supervisor of services

--- lib/servy/supervisor.ex
+++ lib/servy/supervisor.ex
@@ -1,14 +1,15 @@
-defmodule Servy.ServicesSupervisor do
+defmodule Servy.Supervisor do
   use Supervisor
 
-  def start_link(_arg) do
-    IO.puts "Starting the services supervisor..."
+  def start_link do
+    IO.puts "Starting the supervisor..."
     Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
   def init(:ok) do
     children = [
-      Servy.PledgeServer
+      Servy.KickStarter,
+      Servy.ServicesSupervisor
     ]
 
     Supervisor.init(children, strategy: :one_for_one)
