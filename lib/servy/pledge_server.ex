7536d50458ae95375476574fa58ad11cc41cd27a
Jonathan Zarate <zaratedev@gmail.com>
2024-04-29 15:24:42 -0600

    Linking processes

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -79,24 +79,3 @@ defmodule Servy.PledgeServer do
     [{"larry", 400}, {"moe", 500}]
   end
 end
-
-alias Servy.PledgeServer
-
-{:ok, pid} = PledgeServer.start()
-
-send(pid, {:stop, "hammertime"})
-
-IO.inspect PledgeServer.create_pledge("larry", 10)
-
-# PledgeServer.clear()
-
-# IO.inspect PledgeServer.create_pledge("moe", 20)
-# IO.inspect PledgeServer.create_pledge("curly", 30)
-# IO.inspect PledgeServer.create_pledge("daisy", 40)
-# IO.inspect PledgeServer.create_pledge("grace", 50)
-
-IO.inspect PledgeServer.recent_pledges
-
-IO.inspect PledgeServer.total_pledged()
-
-# IO.inspect Process.info(pid, :messages)
